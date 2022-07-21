2eed41bd665b40e71c3559f24d87f9d337dca19a
AndriiHorbanov <doima1998doima1998@gmail.com>
2022-07-21 13:52:06 +0200

    Finished implementing two Time Pickers

--- app/src/main/java/com/example/workinghourscounter/MainActivity.kt
+++ app/src/main/java/com/example/workinghourscounter/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.example.workinghourscounter
 
+
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.text.format.DateFormat
 import com.example.workinghourscounter.databinding.ActivityMainBinding
+import com.example.workinghourscounter.viewModel.MyViewModel
 import java.util.*
 import kotlin.collections.ArrayList
+import android.text.format.DateFormat.is24HourFormat
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+
 
 private lateinit var binding: ActivityMainBinding
 private lateinit var adapter: MyAdapter
 val dataList = ArrayList<DataTime>()
+private lateinit var viewModel: MyViewModel
+val calendar = Calendar.getInstance()
+val hour = calendar.get(Calendar.HOUR_OF_DAY)
+val minet = calendar.get(Calendar.MINUTE)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,22 +31,27 @@ class MainActivity : AppCompatActivity() {
         adapter = MyAdapter()
         binding.recyclerviewView.layoutManager
         binding.recyclerviewView.adapter = adapter
-
-        binding.button.setOnClickListener {
-            if (binding.startWork.text.isNullOrEmpty()) {
-            } else {
-                dataList.add(
-                    DataTime(
-                        binding.startWork.text.toString(),
-                        binding.endWork.text.toString()
-                    )
-                )
-                adapter.setList(dataList)
-            }
-
-
+        binding.startTime.setOnClickListener {
+            openTinePicker()
         }
 
+        binding.endTime.setOnClickListener{
+            openTinePicker()
+        }
+//        binding.button.setOnClickListener {
+//            if (binding.startWork.text.isNullOrEmpty()) {
+//            } else {
+//                dataList.add(
+//                    DataTime(
+//                        binding.startWork.text.toString(),
+//                        binding.endWork.text.toString()
+//                    )
+//                )
+//                adapter.setList(dataList)
+//            }
+//
+//
+//        }
 
 
 //    private fun addListItems(): String{
@@ -44,4 +59,12 @@ class MainActivity : AppCompatActivity() {
 //        return  saved
 //    }
     }
+
+    fun openTinePicker() {
+        val picker = TimePickerDialog(this, TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+        }, hour, minet, false)
+        picker.show()
+    }
+
 }
+
---
+++ app/src/main/java/com/example/workinghourscounter/viewModel/MyViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.workinghourscounter.viewModel
+
+import android.app.TimePickerDialog
+import android.text.format.DateFormat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.android.material.timepicker.TimeFormat
+import java.util.*
+
+class MyViewModel: ViewModel() {
+
+
+
+}
